c2e641b9b0de429668e66d4c6bd0572625d3f3df
Anthony King <anthonydking@slimroms.net>
2015-04-30 22:19:48 +0100

    envsetup: add mk_timer
    this wraps around any method call, mainly for 'make'.
    
    mka needs this on linux, since ionice and schedtool don't
    respect functions
    
    Change-Id: If8cdd235ed9eba377dd90ab8b12e93036a377ea5

--- envsetup.sh
+++ envsetup.sh
@@ -1639,10 +1639,10 @@ function get_make_command()
   echo command make
 }
 
-function make()
+function mk_timer()
 {
     local start_time=$(date +"%s")
-    $(get_make_command) "$@"
+    $@
     local ret=$?
     local end_time=$(date +"%s")
     local tdiff=$(($end_time-$start_time))
@@ -1675,6 +1675,11 @@ function make()
     echo " ####${color_reset}"
     echo
     return $ret
+}
+
+function make()
+{
+    mk_timer $(get_make_command) "$@"
 }
 
 function provision()
