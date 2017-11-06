a6caadde7b0be31cc5753754c5ef9c3fe47b72e5
Ashok Soni <ashok.soni@gmail.com>
2017-11-06 14:17:24 +0530

    Delight logo to a little left.
    Change-Id: I4cb8d0ecbbe71f7dffd34f9ba8986b68e2ca8682

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -483,13 +483,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   #script.Print("Target: %s" % target_fp)
 
-  script.Print("......................................................")
-  script.Print("                                                      ")
-  script.Print("                      BE-DELIGHTED                    ")
-  script.Print("                                                      ")
-  script.Print("                   || ANDROID OREO ||                 ")
-  script.Print("                                                      ")
-  script.Print("......................................................")
+  script.Print("....................................................")
+  script.Print("                                                    ")
+  script.Print("                    BE-DELIGHTED                    ")
+  script.Print("                                                    ")
+  script.Print("                 || ANDROID OREO ||                 ")
+  script.Print("                                                    ")
+  script.Print("....................................................")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
